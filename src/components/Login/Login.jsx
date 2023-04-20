63dca1e97e14972d66ae52d21ed6e9968fb97bec
BidhanCR <bidhanroy300805@gmail.com>
2023-04-20 17:43:44 +0600

    componentsadded

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
+import "./Login.css";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const Login = () => {
-    return (
-        <div>
-            <h2>Login coming sooon</h2>
+  const {logIn} = useContext(AuthContext);
+  const [error, setError] = useState('');
+
+  const navigate = useNavigate();
+
+  const location = useLocation();
+  console.log(location)
+
+  const from = location.state?.from?.pathname || '/';
+  const handleLogIn = event => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    setError('');
+    
+    logIn(email, password)
+    .then(result => {
+      const loggedUser = result.user;
+      console.log(loggedUser);
+      form.reset();
+      navigate(from, {replace: true});
+    })
+    .catch(error => {
+      setError(error.message)
+    })
+  }
+  return (
+    <div className="form-container">
+      <h2 className="form-title">Please Login here</h2>
+      <form onSubmit={handleLogIn}>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="" required />
         </div>
-    );
+        <div className="form-control">
+          <label htmlFor="password">password</label>
+          <input type="password" name="password" id="" required />
+        </div>
+        <div className="form-control">
+          <input className="btn-submit" type="submit" value="Login" />
+        </div>
+      </form>
+      <p><small>New to Ema-John? <Link to="/sign-up">Create New Account</Link></small></p>
+      <p className="text-error">{error}</p>
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArweuGugV7p8wCj98tUvlgmG0H7d5T_Xc",
+  authDomain: "ema-john-with-firebase-a-23432.firebaseapp.com",
+  projectId: "ema-john-with-firebase-a-23432",
+  storageBucket: "ema-john-with-firebase-a-23432.appspot.com",
+  messagingSenderId: "800452895797",
+  appId: "1:800452895797:web:eb9421fea0e124bb3cfd7c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
